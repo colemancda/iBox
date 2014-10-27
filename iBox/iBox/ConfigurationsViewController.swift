f449a0c435f029dfaaff74488d02eaacf0c838af
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-10-27 19:38:38 -0400

    created project

---
+++ iBox/iBox/ConfigurationsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ConfigurationsViewController.swift
+//  iBox
+//
+//  Created by Alsey Coleman Miller on 10/27/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+import UIKit
+
+class ConfigurationsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
