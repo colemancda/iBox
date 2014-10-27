a9fb1f371eea49fa09c2a0a1259fd69d238c8018
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-10-27 19:46:06 -0400

    created BochsKit framework

---
+++ iBox/BochsKit/BochsKit/BochsKit.h
@@ -0,0 +1,19 @@
+//
+//  BochsKit.h
+//  BochsKit
+//
+//  Created by Alsey Coleman Miller on 10/27/14.
+//  Copyright (c) 2014 Bochs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BochsKit.
+FOUNDATION_EXPORT double BochsKitVersionNumber;
+
+//! Project version string for BochsKit.
+FOUNDATION_EXPORT const unsigned char BochsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BochsKit/PublicHeader.h>
+
+
--- iBox/iBox/ConfigurationsViewController.swift
+++ iBox/iBox/ConfigurationsViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class ConfigurationsViewController: UITableViewController {
     
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Loading
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    // MARK: - Memory Warning
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     
 }
