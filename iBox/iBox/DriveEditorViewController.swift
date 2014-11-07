db14426289118db559386a259ff21942c6537880
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-11-07 00:21:37 -0500

    added TableViewCell.swift

--- iBox/iBox/DriveEditorViewController.swift
+++ iBox/iBox/DriveEditorViewController.swift
@@ -7,16 +7,49 @@
 //
 
 import UIKit
+import CoreData
 
-class DriveEditorViewController: UITableViewController {
-    
-    // MARK: - IB Outlets
-    
-    
+class DriveEditorViewController: UITableViewController, UITextFieldDelegate {
     
     // MARK: - Properties
     
-    var drive: Drive?
+    var drive: Drive? {
+        
+        didSet {
+            
+            if drive != nil {
+                
+                let driveEntity = DriveEditorViewControllerDriveEntity(rawValue: drive!.entity.name!)!
+                
+                self.updateTableViewCellLayoutForEntity(driveEntity)
+            }
+        }
+    }
+    
+    // MARK: - Private Properties
+    
+    private var tableViewCellLayout = [[DriveEditorViewControllerTableViewCellItem]]()
+    
+    // MARK: - Private Methods
+    
+    private func updateTableViewCellLayoutForEntity(entity: DriveEditorViewControllerDriveEntity) {
+        
+        // create layout based on entity
+        
+        switch entity {
+            
+        case .CDRom:
+            
+            self.tableViewCellLayout = [[.FileName], [.DiscInserted]]
+            
+        case .HardDiskDrive:
+            
+            self.tableViewCellLayout = [[.FileName], [.Headers, .Cylinders, .TracksPerSector]]
+        }
+        
+        // reload UI
+        self.tableView.reloadData()
+    }
     
     // MARK: - Actions
     
@@ -24,4 +57,131 @@ class DriveEditorViewController: UITableViewController {
         
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    @IBAction func switchFlipped(sender: UISwitch) {
+        
+        
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return self.tableViewCellLayout.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // get model object
+        let sectionArray = self.tableViewCellLayout[section]
+        
+        return sectionArray.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // get model object
+        let sectionArray = self.tableViewCellLayout[indexPath.section]
+        let cellItem = sectionArray[indexPath.row]
+        
+        // get and configure cell
+        switch cellItem {
+            
+        case .FileName:
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier(DriveEditorViewControllerTableViewCellReusableIdentifier.FileNameCell.rawValue, forIndexPath: indexPath) as TextFieldCell
+            
+            cell.titleLabel.text = NSLocalizedString("File Name", comment: "File Name")
+            
+            cell.textField.text = self.drive!.fileName
+            
+            return cell
+            
+        case .DiscInserted:
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier(DriveEditorViewControllerTableViewCellReusableIdentifier.SwitchCell.rawValue, forIndexPath: indexPath) as UITableViewCell
+            
+            cell.textLabel.text = NSLocalizedString("Disc Inserted", comment: "Disc Inserted")
+            
+            /*
+            let switchControl = UISwitch()
+            switchControl.addTarget(self, action: "switchFlipped:", forControlEvents: UIControlEvents.ValueChanged)
+            cell.accessoryView = switchControl
+            */
+            
+            (cell.accessoryView as UISwitch).on = (self.drive as CDRom).discInserted.boolValue
+
+        case .Headers:
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier(DriveEditorViewControllerTableViewCellReusableIdentifier.NumberInputCell.rawValue, forIndexPath: indexPath) as TextFieldCell
+            
+            cell.titleLabel.text = NSLocalizedString("Headers", comment: "Headers")
+            
+            cell.
+            
+        default:
+            return tableView.dequeueReusableCellWithIdentifier(NSStringFromClass(UITableViewCell)!) as UITableViewCell
+        }
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        switch section {
+            
+        case 0:
+            
+            return NSLocalizedString("Info", comment: "Info")
+            
+        case 1:
+            
+            let driveEntity = DriveEditorViewControllerDriveEntity(rawValue: drive!.entity.name!)!
+            
+            switch driveEntity {
+                
+            case .CDRom: return NSLocalizedString("CDROM Configuration", comment: "CDROM Configuration")
+                
+            case .HardDiskDrive : return NSLocalizedString("HDD Configuration", comment: "HDD Configuration")
+                
+            default: return "Drive"
+                
+            }
+            
+        default: return "Section"
+            
+        }
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
+        
+        
+    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        return true
+    }
+}
+
+// MARK: - Enumerations
+
+private enum DriveEditorViewControllerDriveEntity: String {
+    
+    case CDRom = "CDRom"
+    case HardDiskDrive = "HardDiskDrive"
+}
+
+private enum DriveEditorViewControllerTableViewCellItem {
+    
+    case FileName, DiscInserted, Headers, Cylinders, TracksPerSector
+}
+
+private enum DriveEditorViewControllerTableViewCellReusableIdentifier: String {
+    
+    case FileNameCell = "FileNameCell"
+    case SwitchCell = "SwitchCell"
+    case NumberInputCell = "NumberInputCell"
 }
