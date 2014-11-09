59d0090c024a4fb5cb7a3d450ae75bff1f5eaa29
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-11-09 03:41:29 -0500

    working on FileSelectionVC

--- iBox/iBox/FileSelectionViewController.swift
+++ iBox/iBox/FileSelectionViewController.swift
@@ -51,13 +51,18 @@ class FileSelectionViewController: UITableViewController {
         
         // create alert controller
         let alertController = UIAlertController(title: NSLocalizedString("Create New HDD Image", comment: "Create New HDD Image Alert Controller Title"),
-            message: NSLocalizedString("Specify a name", comment: "Create New HDD Image Alert Controller Message"),
+            message: NSLocalizedString("Specify a size (in MB) and a name", comment: "Create New HDD Image Alert Controller Message"),
             preferredStyle: UIAlertControllerStyle.Alert)
         
-        // add text field
+        // add text fields
         alertController.addTextFieldWithConfigurationHandler { (textField: UITextField!) -> Void in
             
             textField.text = "hddImage"
+        }
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField: UITextField!) -> Void in
+            
+            textField.text = "1024"
         }
         
         // add create and cancel button
@@ -68,13 +73,51 @@ class FileSelectionViewController: UITableViewController {
         
         alertController.addAction(UIAlertAction(title: NSLocalizedString("Create", comment: "Create"), style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) -> Void in
             
+            // TODO: should probably validate file name (e.g. no spaces or invalid characters) and size text
+            
             // dismiss and show progress view
             alertController.dismissViewControllerAnimated(true, completion: { () -> Void in
                 
                 let progressHUD = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
                 
                 // configure HUD
-                progressHUD.mode = 
+                progressHUD.labelText = NSLocalizedString("Creating image...", comment: "'Creating image...' Progress HUD text")
+                
+                // create image... 
+                
+                let textField = alertController.textFields!.first as UITextField
+                
+                let fileName = textField.text + ".img"
+                
+                let fileURL = documentsURL.URLByAppendingPathComponent(fileName)
+                
+                let size = (alertController.textFields![1] as UITextField).text.toInt()!
+                
+                BXImage.createImageWithURL(fileURL, sizeInMB: UInt(size), completion: { (success: Bool) -> Void in
+                    
+                    if !success {
+                        
+                        // hide progress HUD and show alert view
+                        MBProgressHUD.hideHUDForView(self.view, animated: true)
+                        
+                        let alertView = UIAlertController(title: NSLocalizedString("Error", comment: "Error"),
+                            message: NSLocalizedString("Could not create the image", comment: "Could not create the image"),
+                            preferredStyle: UIAlertControllerStyle.Alert)
+                        
+                        alertView.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel"), style: UIAlertActionStyle.Cancel, handler: { (action: UIAlertAction!) -> Void in
+                            
+                            alertView.dismissViewControllerAnimated(true, completion: nil)
+                        }))
+                        
+                        return
+                    }
+                    
+                    // create new entity
+                    
+                    
+                    // hide progress HUD
+                    MBProgressHUD.hideHUDForView(self.view, animated: true)
+                })
             })
         }))
         
